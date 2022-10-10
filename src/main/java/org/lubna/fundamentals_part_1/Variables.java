a581d2d9f5c95b689dbcd21df07cebdad2ab7df2
lubna farheen <lubnafarheensweden@gmail.com>
2022-10-10 23:13:28 +0200

    Initial commit adding project structure

---
+++ src/main/java/org/lubna/fundamentals_part_1/Variables.java
@@ -0,0 +1,9 @@
+package org.lubna.fundamentals_part_1;
+
+public class Variables {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 12;
+        System.out.println(x + y);
+    }
+}
